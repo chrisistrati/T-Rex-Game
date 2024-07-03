76c4fb4b5061d9ddbc9f14920d8b2ee052b13b9c
chrisistrati <chris.istrati@gmail.com>
2024-07-03 13:12:19 +0300

    Added jump function, changes length by function of time

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ import numpy as np
 import cv2
 import time
 
+def jump(image, temlpate):
+    result=cv2.matchTemplate(image, temlpate, cv2.TM_CCOEFF_NORMED)
+    min_value, max_value, min_loc, max_loc = cv2. minMaxLoc(result)
+    if max_value>0.6:
+        pyautogui.press("up")
+def checkObject(image, temlpate):
+    answer=False
+    result=cv2.matchTemplate(image, temlpate, cv2.TM_CCOEFF_NORMED)
+    min_value, max_value, min_loc, max_loc = cv2. minMaxLoc(result)
+    if max_value>0.6:
+        answer=True
+    return answer
+
 big_cactus=cv2.imread("images/big_cactus.png")
 small_cactus=cv2.imread("images\small_cacuts.png")
+small_cactus_group=cv2.imread("images\small_cactus_group.png")
+cursor = cv2.imread("images\cursor.png")
 
 time.sleep(1)
 image=np.array(pyautogui.screenshot())
 dino=cv2.imread("images\dinosaur.png")
 result=cv2.matchTemplate(image, dino, cv2.TM_CCOEFF_NORMED)
 
-cv2.imshow("result", result)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# cv2.imshow("result", result)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 
-time.sleep(2)
+# time.sleep(2)
 
 min_value, max_value, min_loc, max_loc = cv2. minMaxLoc(result)
 
@@ -27,21 +42,21 @@ h=dino.shape[0]
 # cv2.waitKey()
 # cv2.destroyAllWindows()
 
-smol_image=np.array(pyautogui.screenshot(region=(max_loc[0], max_loc[1], w+100, h)))
-cv2.imshow("wrgt", smol_image)
-cv2.waitKey()
-cv2.destroyAllWindows()
-
+smol_image=np.array(pyautogui.screenshot(region=(max_loc[0], max_loc[1], w+150, h)))
+# cv2.imshow("wrgt", smol_image)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+start=time.time()
 while True:
-    smol_image=np.array(pyautogui.screenshot(region=(max_loc[0], max_loc[1], w+100, h)))
-    result_big=cv2.matchTemplate(smol_image, big_cactus, cv2.TM_CCOEFF_NORMED)
-    min_value_big, max_value_big, min_loc_big, max_loc_big = cv2. minMaxLoc(result_big)
-    print(max_value_big)
-    if max_value_big>0.8:
-        pyautogui.press("space")
-        print("runnig")
-    result_small=cv2.matchTemplate(smol_image, small_cactus, cv2.TM_CCOEFF_NORMED)
-    min_value_small, max_value_small, min_loc_small, max_loc_small = cv2. minMaxLoc(result_small)
-    if max_value_small>0.8:
-        pyautogui.press("space")
-        print("runnig")
+    end=time.time()
+    duration=end-start
+    if duration <=20:
+        smol_image=np.array(pyautogui.screenshot(region=(max_loc[0], max_loc[1], w+150, h)))
+    elif duration >=20:
+        smol_image=np.array(pyautogui.screenshot(region=(max_loc[0], max_loc[1], w+200, h)))
+    jump(smol_image, big_cactus)
+    jump(smol_image, small_cactus)    
+        
+    
+
+    
